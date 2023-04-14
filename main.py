ccbe5f0ebbc9b1b85fa395e492d0407b0b844613
magicsteev <121175364+magicsteev@users.noreply.github.com>
2023-04-14 17:45:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from requests import get
 class GetRewards:
 
     def __init__(self):
-
+        #endpoint cosmosHub
         self.rest_servers_prod = ['https://api-cosmoshub-ia.cosmosia.notional.ventures',
                              'https://cosmos-lcd.quickapi.com:443', 'https://lcd-cosmoshub.blockapsis.com',
                              'https://lcd.cosmos.dragonstake.io', 'https://api.cosmos.silknodes.io',
                              'https://cosmoshub-api.lavenderfive.com:443']
-
+         #endpoint osmosis
+        self.rest_servers_osmo = ['https://rest.cosmos.directory/osmosis']
+        
         self.wallet_address = "cosmos13w3uzqu8yjups2r4rgw7cy0jxe9nya5966j68g"
         self.denom = "uatom"
 
