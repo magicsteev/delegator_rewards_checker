870ebc7953e3adafa2595f1bb76443709553a562
magicsteev <121175364+magicsteev@users.noreply.github.com>
2023-04-16 17:56:23 +0200

    Update update_sheet.py

--- update_sheet.py
+++ update_sheet.py
@@ -15,11 +15,7 @@ class SheetRewards:
 
     def updtsheet(self):
       sheet = self.client.open('CosmosRewards').sheet1
-      
-      
-      
-      print(f"La première ligne vide est la ligne {first_empty_row}")
-      
+
       #Remplir le tableau à partir de la première ligne disponible
       json_obj = json.loads(self.rewards)
       for reward in json_obj['rewards']:
@@ -28,11 +24,11 @@ class SheetRewards:
         # Trouver la première ligne vide dans la première colonne
         cell = sheet.find("")
         sheet.update_cell(cell.row, 1, timestamp)
-        sheet.update_cell(row_to_update.row, 2, reward['validator_address'])
-        for reward in reward['validator_address']:
-            sheet.update_cell(row_to_update.row, 3, reward['amount'])
-            sheet.update_cell(row_to_update.row, 4, reward['amount'])
+        sheet.update_cell(cell.row, 2, reward['validator_address'])
+        for r in reward['reward']:
+            sheet.update_cell(cell.row, 3, reward['amount'])
+            sheet.update_cell(cell.row, 4, reward['denom'])
         
         
-      sheet.update('A2:B3', [[1, 2], [3, 4]])
+      #sheet.update('A2:B3', [[1, 2], [3, 4]])
       print("sheetupdated:")
