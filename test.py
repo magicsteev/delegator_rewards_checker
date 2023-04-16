b0a0e856cc7555eb72e2a484e15effa69a865ee7
magicsteev <121175364+magicsteev@users.noreply.github.com>
2023-04-16 19:42:48 +0200

    Update test.py

--- test.py
+++ test.py
@@ -7,17 +7,53 @@ import datetime
 # Write Python 3 code in this online editor and run it.
 print("Hello world")
 timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
+#ATOM
 a=GetRewards("https://api-cosmoshub-ia.cosmosia.notional.ventures/", "cosmos1lpu6xj6qsu5aqxnserzxjteaq56j86lcl9hchy", "uatom")
 r=a.get_pending_rewards()
 print("new_rewards:", r)
 
 b=SheetRewards(r, timestamp)
 t=b.updtsheet()
-
+#OSMO
 a=GetRewards("https://rest.cosmos.directory/osmosis/", "osmo1lpu6xj6qsu5aqxnserzxjteaq56j86lch7ygpk", "uosmo")
 r=a.get_pending_rewards()
 print("new_rewards:", r)
 
 b=SheetRewards(r, timestamp)
 t=b.updtsheet()
+
+
+#EVMOS
+a=GetRewards("https://rest.cosmos.directory/evmos", "evmos184ceng8y5n70wtegmmchefa4uynsssl2hgulld", "uevmos")
+r=a.get_pending_rewards()
+print("new_rewards:", r)
+
+b=SheetRewards(r, timestamp)
+t=b.updtsheet()
+
+#SECRET
+#https://rest.cosmos.directory/secretnetwork
+#secret1q4x50wc7tjz8y6vchz54rxeahtv4v6srj5etrq
+
+a=GetRewards("https://rest.cosmos.directory/secretnetwork", "secret1q4x50wc7tjz8y6vchz54rxeahtv4v6srj5etrq", "usecret")
+r=a.get_pending_rewards()
+print("new_rewards:", r)
+
+b=SheetRewards(r, timestamp)
+t=b.updtsheet()
+
+#AKT
+#https://rest.cosmos.directory/akash
+#akash1lpu6xj6qsu5aqxnserzxjteaq56j86lcj76lw7
+
+a=GetRewards("https://rest.cosmos.directory/akash", "akash1lpu6xj6qsu5aqxnserzxjteaq56j86lcj76lw7", "uakt")
+r=a.get_pending_rewards()
+print("new_rewards:", r)
+
+b=SheetRewards(r, timestamp)
+t=b.updtsheet()
+
+
+#STARGAZE
+#https://rest.cosmos.directory/stargaze
+#
