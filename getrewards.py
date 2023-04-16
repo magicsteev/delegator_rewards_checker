b7253bc8f346e92f99e5b28d8036c385ea65b9b3
magicsteev <121175364+magicsteev@users.noreply.github.com>
2023-04-16 11:50:59 +0200

    Update getrewards.py

--- getrewards.py
+++ getrewards.py
@@ -2,16 +2,14 @@ from requests import get
 
 class GetRewards:
 
-    def __init__(self):
+    def __init__(self, endpoint, address, denom):
         #endpoint cosmosHub
-        self.rest_servers_prod = ['https://api-cosmoshub-ia.cosmosia.notional.ventures',
-                             'https://cosmos-lcd.quickapi.com:443', 'https://lcd-cosmoshub.blockapsis.com',
-                             'https://lcd.cosmos.dragonstake.io', 'https://api.cosmos.silknodes.io',
-                             'https://cosmoshub-api.lavenderfive.com:443']
+        self.rest_servers_prod = endpoint
          #endpoint osmosis
         self.rest_servers_osmo = ['https://rest.cosmos.directory/osmosis']
         
-        self.wallet_address = "cosmos13w3uzqu8yjups2r4rgw7cy0jxe9nya5966j68g"
-        self.denom = "uatom"
+        self.wallet_address = address
+        self.denom = denom
 
         self.rewards_url = f"/cosmos/distribution/v1beta1/delegators/{self.wallet_address}/rewards"
+        print("End Cons")
