3df6144cc01860c48b2af8f7b142c2d8c65315ca
magicsteev <121175364+magicsteev@users.noreply.github.com>
2023-04-16 17:25:22 +0200

    Update update_sheet.py

--- update_sheet.py
+++ update_sheet.py
@@ -1,11 +1,12 @@
 import gspread
+import json
 from oauth2client.service_account import ServiceAccountCredentials
 
 class SheetRewards:
 
     def __init__(self, rewards):
         #endpoint cosmosHub
-        self.rewards = rewards
+         = rewards
         # Définir les informations d'identification pour accéder à votre compte Google Sheet
         scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
         creds = ServiceAccountCredentials.from_json_keyfile_name('cosmosrewards-d891d19a5027.json', scope)
@@ -14,5 +15,8 @@ class SheetRewards:
 
     def updtsheet(self):
       sheet = self.client.open('CosmosRewards').sheet1
+      json_obj = json.loads(self.rewards)
+      for reward in json_obj['rewards']:
+        print("Validator address: ", reward['validator_address'])
       sheet.update('A2:B3', [[1, 2], [3, 4]])
       print("sheetupdated:")
