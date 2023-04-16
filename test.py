225232e84ea24734fa991473519551843dd95828
magicsteev <121175364+magicsteev@users.noreply.github.com>
2023-04-16 13:20:56 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,19 @@
 from getrewards import GetRewards
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import csv
+
+# Définir les informations d'identification pour accéder à votre compte Google Sheet
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+client = gspread.authorize(creds)
+
+# Ouvrir la feuille de calcul
+sheet = client.open('CosmosRewards').sheet1
+data = [    {'Nom': 'Durand', 'Prénom': 'Jean', 'Âge': 42},    {'Nom': 'Dupont', 'Prénom': 'Marie', 'Âge': 28},    {'Nom': 'Martin', 'Prénom': 'Pierre', 'Âge': 35},]
+sheet.insert_rows(data)
+
+
 # Online Python compiler (interpreter) to run Python online.
 # Write Python 3 code in this online editor and run it.
 print("Hello world")
