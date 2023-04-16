5f0bf840d2d5a64ff83b60bf682d430973b9408e
magicsteev <121175364+magicsteev@users.noreply.github.com>
2023-04-16 12:01:41 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,3 +3,5 @@ from getrewards import GetRewards
 # Write Python 3 code in this online editor and run it.
 print("Hello world")
 a=GetRewards("https://api-cosmoshub-ia.cosmosia.notional.ventures/", "cosmos1lpu6xj6qsu5aqxnserzxjteaq56j86lcl9hchy", "uatom")
+r=a.get_pending_rewards()
+print("new_rewards:", r)
