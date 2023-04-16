ebb6fa11377e9b791b186d70cc3d41b1196b1df9
magicsteev <121175364+magicsteev@users.noreply.github.com>
2023-04-16 17:11:21 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,6 @@
 from getrewards import GetRewards
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from update_sheet import SheetRewards
 import csv
-
-# Définir les informations d'identification pour accéder à votre compte Google Sheet
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('cosmosrewards-d891d19a5027.json', scope)
-client = gspread.authorize(creds)
-
-# Ouvrir la feuille de calcul
-sheet = client.open('test').sheet1
-data = ["Zayn","Malik","12","20"] 
-sheet.update('A1:B2', [[1, 2], [3, 4]])
-print("celllval:", sheet.acell('B1').value)
-
-#sheet.insert_rows(data)
-
 
 # Online Python compiler (interpreter) to run Python online.
 # Write Python 3 code in this online editor and run it.
@@ -23,3 +8,6 @@ print("Hello world")
 a=GetRewards("https://api-cosmoshub-ia.cosmosia.notional.ventures/", "cosmos1lpu6xj6qsu5aqxnserzxjteaq56j86lcl9hchy", "uatom")
 r=a.get_pending_rewards()
 print("new_rewards:", r)
+
+b=SheetRewards(r)
+t=updtsheet(b)
