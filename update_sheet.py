c44ca92a23af153ad0a59c3b65c9ec249a28ecec
magicsteev <121175364+magicsteev@users.noreply.github.com>
2023-04-16 18:29:54 +0200

    Update update_sheet.py

--- update_sheet.py
+++ update_sheet.py
@@ -11,11 +11,12 @@ class SheetRewards:
         scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
         creds = ServiceAccountCredentials.from_json_keyfile_name('cosmosrewards-d891d19a5027.json', scope)
         self.client = gspread.authorize(creds)
-
-
+    
     def updtsheet(self):
       sheet = self.client.open('CosmosRewards').sheet1
-
+      str_list = list(filter(None, sheet.col_values(1)))
+      cell=str(len(str_list)+1)
+      print("Cell : ", cell)
       #Remplir le tableau à partir de la première ligne disponible
       json_obj = json.loads(self.rewards)
       for reward in json_obj['rewards']:
@@ -23,7 +24,7 @@ class SheetRewards:
         
         # Trouver la première ligne vide dans la première colonne
         #cell = sheet.find('')
-        cell = next_available_row(sheet)
+
         sheet.update_cell(cell.row, 1, timestamp)
         sheet.update_cell(cell.row, 2, reward['validator_address'])
         for r in reward['reward']:
