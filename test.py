8b7389453a336f38784d3bf56453cdbbbe880d20
magicsteev <121175364+magicsteev@users.noreply.github.com>
2023-04-16 11:58:03 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ from getrewards import GetRewards
 # Online Python compiler (interpreter) to run Python online.
 # Write Python 3 code in this online editor and run it.
 print("Hello world")
-a=GetRewards("endpoint", "address", "uatom")
+a=GetRewards("https://api-cosmoshub-ia.cosmosia.notional.ventures/", "cosmos1lpu6xj6qsu5aqxnserzxjteaq56j86lcl9hchy", "uatom")
