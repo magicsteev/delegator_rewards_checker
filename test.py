fbc5d2042e97393df5cb3de3c08d686109eeed7e
magicsteev <121175364+magicsteev@users.noreply.github.com>
2023-06-22 14:59:19 +0200

    Update test.py

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 #ATOM
 a=GetRewards("https://api-cosmoshub-ia.cosmosia.notional.ventures/", "cosmos1lpu6xj6qsu5aqxnserzxjteaq56j86lcl9hchy", "uatom")
 r=a.get_pending_rewards()
+v=a.get_validator_name("cosmosvaloper1gpx52r9h3zeul45amvcy2pysgvcwddxrgx6cnv")
 print("new_rewards:", r)
 
 b=SheetRewards(r, timestamp)
